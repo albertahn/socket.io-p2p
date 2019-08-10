629405520f06bfb5e71dc50d9f586d3b805e06dc
albertpoynt <albert@poynt.co>
2019-08-10 12:23:22 -0700

    alberto's socketio with video example for live stuff

--- examples/streaming/src/index.js
+++ examples/streaming/src/index.js
@@ -7,27 +7,54 @@ var socket = io()
 var p2p = new P2P(socket)
 var startButton = document.getElementById('start-stream')
 
+var video = document.createElement('video')
+    document.body.appendChild(video)
+
+    var constraints = { audio: true, video: true };
+    navigator.mediaDevices.getUserMedia(constraints)
+    .then(function(stream) {
+      console.log("got stream");
+    })
+    .catch(function(err) {
+      /* handle the error */
+      console.log(err);
+    });
+
 p2p.on('start-stream', function () {
   p2p.usePeerConnection = true
   startButton.setAttribute('disabled', true)
 })
 
 p2p.on('stream', function (stream) {
-  var audio = document.querySelector('audio')
-  audio.src = window.URL.createObjectURL(stream)
-  audio.play()
+  console.log("streaming now:"+ stream);
+  //var audio = document.querySelector('audio')
+  //audio.src = window.URL.createObjectURL(stream)
+  //audio.play()
+
+  // got remote video stream, now let's show it in a video tag
+                var video = document.querySelector('video')
+
+                if ('srcObject' in video) {
+                  video.srcObject = stream
+                } else {
+                  video.src = window.URL.createObjectURL(stream) // for older browsers
+                }
+
+                video.play()
+
 })
 
 function startStream () {
   startButton.setAttribute('disabled', true)
-  navigator.getUserMedia({ audio: true }, function (stream) {
+  navigator.getUserMedia({ audio: true, video: true }, function (stream) {
     var audioContext = new window.AudioContext()
     var mediaStreamSource = audioContext.createMediaStreamSource(stream)
     var mediaStreamDestination = audioContext.createMediaStreamDestination()
     mediaStreamSource.connect(mediaStreamDestination)
 
     var socket = io()
-    var p2p = new P2P(socket, {peerOpts: {stream: mediaStreamDestination.stream}})
+    //var p2p = new P2P(socket, {peerOpts: {stream: mediaStreamDestination.stream}})
+    var p2p = new P2P(socket, {peerOpts: {stream: stream}})
 
     p2p.on('ready', function () {
       p2p.usePeerConnection = true
